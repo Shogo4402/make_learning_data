5428c0b1d9bb36cbd97ab7daff8f9d7334704b78
Shogo <4402485@gmail.com>
2022-07-05 11:02:02 +0000

    new

--- src/making_data.cpp
+++ src/making_data.cpp
@@ -4,6 +4,10 @@
 #include <thread>
 #include <chrono>
 #include <fstream>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 #include "std_msgs/Float64.h"
 #include "std_msgs/Float64MultiArray.h"
 
@@ -24,6 +28,8 @@ void set_power(int n){
 }
 
 void file_write(wheel_speed_freqs freqs){
+	freqs.left_hz = 0;
+	freqs.right_hz = 0;
 
 	std::ofstream writing_file_left;
 	std::ofstream writing_file_right;
@@ -50,18 +56,44 @@ class SubPub{
 public:
 	ros::NodeHandle n;
 	ros::Publisher pub;
-	ros::Subscriber sub1,sub2;
+	ros::Subscriber sub1,sub2,sub3;
+	int judge_angular=0,judge_camera=0;
+	double pre_data,THETA,DELTA_THETA,NUM,X,Y,NUMBER=0;
 	void callback_init(const std_msgs::Float64MultiArray &init_value);
 	void callback_getting_angular(const std_msgs::Float64MultiArray &angular_data);
-	double pre_data;
+	void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+
 	SubPub(){
-		SubPub::sub1 = n.subscribe("init_info_data",10,&SubPub::callback_init,this);
-		SubPub::pub = n.advertise<std_msgs::Float64>("/pre_pose_topic",10);
+		SubPub::sub1 = n.subscribe("init_info_data",1,&SubPub::callback_init,this);
+		SubPub::pub = n.advertise<std_msgs::Float64>("/pre_pose_topic",1);
+		SubPub::sub2 = n.subscribe("/imu/vel_pos_est",1,&SubPub::callback_getting_angular,this); 
+		SubPub::sub3 = n.subscribe("/usb_cam/image_raw", 1, &SubPub::imageCallback,this);
 	};
 };
 
 void SubPub::callback_getting_angular(const std_msgs::Float64MultiArray &angular_data){
-	pre_data = angular_data.data[2];
+	if(judge_angular==1){
+		pre_data = angular_data.data[2];
+		ROS_INFO("%lf",pre_data);
+		judge_angular = 0;
+	}
+}
+
+void SubPub::imageCallback(const sensor_msgs::ImageConstPtr& msg_image){
+	if(judge_camera==1){
+		cv_bridge::CvImagePtr cv_ptr;
+		//cv::waitKey(1000);
+		try{
+			cv_ptr = cv_bridge::toCvCopy(msg_image, sensor_msgs::image_encodings::BGR8);
+		}
+		catch (cv_bridge::Exception& ex){
+			ROS_ERROR("error");
+			exit(-1);
+		}
+		cv::imwrite("/home/ubuntu/image_data/image0.png",cv_ptr->image);
+		cv::waitKey(10);
+		judge_camera = 0;
+	}
 }
 
 void SubPub::callback_init(const std_msgs::Float64MultiArray &init_value){
@@ -69,11 +101,22 @@ void SubPub::callback_init(const std_msgs::Float64MultiArray &init_value){
 	int i;
 	wheel_speed_freqs freqs;
 
+	std::ofstream writing_file;
+        std::string filename = "/home/ubuntu/text_files/sample.txt";
+        writing_file.open(filename, std::ios::app);
 	//(1)input data favorite pose(start_theta,delta_theta,x,y)
 	//motor on
 	set_power(1);
-	std_msgs::Float64 msg;
-	msg.data = init_value.data[0];
+	std_msgs::Float64 msg_theta;
+	msg_theta.data = init_value.data[0]*pi/180;
+	//pre_data = msg_theta.data;
+
+	THETA = init_value.data[0];
+	DELTA_THETA = init_value.data[1];
+	NUM = init_value.data[2];
+	X = init_value.data[3];
+	Y = init_value.data[4];
+
 	
 	//(2)decide omega t  of motor
 	int TIME = 200;
@@ -82,26 +125,31 @@ void SubPub::callback_init(const std_msgs::Float64MultiArray &init_value){
 	freqs.right_hz = -freqs.left_hz;
 	//(3)camera on
 		
-	for(i=0;i<int(init_value.data[2]);i++){
+	for(i=0;i<int(NUM);i++){
 		//(4)imu_offset off
-		pub.publish(msg);
+		pub.publish(msg_theta);
 		//(5)motor start & stop
 		input_raw_freqs(freqs,TIME);
 		
 		//(6)get imu data
-		sub2 = n.subscribe("/imu/vel_pos_est",10,&SubPub::callback_getting_angular,this); 
-		
+		judge_angular = 1;
+		judge_camera = 1;
+		ros::spinOnce();
+		ROS_INFO("%lf",pre_data);
 		//(7)get camera data 
+
 		//(8)pre_pose start->(5)
-		msg.data = pre_data;
+		msg_theta.data = pre_data;
 
 		//(9)save_data
+		writing_file << i << "\t" << X << "\t" << Y << "\t"<< pre_data<<std::endl;
 
 		//(10)break time
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
 	//(11)finish motor off and camera off
 	set_power(0);
+	writing_file.close();
 	
 }
 
